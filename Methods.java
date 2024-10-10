7b259662333d497597651db0130a36b92ab5a669
Polaka Pavan Kumar <129263679+Pavan779786@users.noreply.github.com>
2024-10-10 10:28:54 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,19 @@
+package pavan;
+
+public class Methods {
+	public static void m1() {
+		System.out.println("this is method 1");
+	}
+public static void m2() {
+	System.out.println("this is method 2");
+}
+	
+
+       public static void main (String[]args) {
+    	   System.out.println("hii , welcome to the methods");
+    	   m1();
+    	   m2();
+    	   
+    	   
+       }
+}
